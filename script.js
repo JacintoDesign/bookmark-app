22869c34d10e39423f98f7e426e22aa9fb73ddef
JACINTO WONG <jacinto.wong@cbc.ca>
2020-02-21 14:28:10 -0500

    basic modal functionality

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+const closeIcon = document.getElementById('close-modal');
+const deleteIcon = document.getElementById('delete-bookmark');
+const modal = document.getElementById('modal');
+const show = document.getElementById('show');
 
-const deleteEl = document.getElementById('close-icon');
 
-deleteEl.addEventListener('click', deleteBookmark);
+
+deleteIcon.addEventListener('click', deleteBookmark);
+
+// Dismiss Modal
+closeIcon.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Show Modal 
+show.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Remove Modal clicking outside of it
+window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
 
 
 function deleteBookmark() {
